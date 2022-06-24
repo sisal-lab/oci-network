e87938e93ca8f9a7b9b519173fd24ccf6a6932df
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-06-24 20:23:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,16 @@ terraform {
   }
 }
 
+locals {
+  compartment_id = data.tfe_outputs.oci_compartment.compartment.id
+}
+
 data "tfe_outputs" "oci_compartment" {
   organization = var.tfe_org
   workspace = var.workspace_oci_compartment
+}
+
+resource "oci_core_vcn" "main" {
+  #Required
+  compartment_id = local.compartment_id
 }
